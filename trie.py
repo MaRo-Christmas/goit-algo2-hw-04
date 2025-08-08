9fc2485a4d73ae7439e5394e479db534cf6496fc
Maryna_Rozhdestvenska <rozhd.buro@gmail.com>
2025-08-09 01:25:43 +0300

    Initial commit

---
+++ trie.py
@@ -0,0 +1,24 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def put(self, word, index):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def get(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return None
+            node = node.children[char]
+        return node if node.is_end_of_word else None
